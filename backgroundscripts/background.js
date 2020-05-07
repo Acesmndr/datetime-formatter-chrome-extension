43e96b823a0f2446f21b9cc711569e67d829751e
acesmndr <acesmndr@gmail.com>
2020-05-07 22:41:01 +0545

    Reduce permissions

--- backgroundscripts/background.js
+++ backgroundscripts/background.js
@@ -1,37 +1,17 @@
-const dateRegex = {
-  type0: '[0-9]{13}',
-  type1: '[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} (\\+|\\-)[0-9]{4}',
-  type2: '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z',
-  type3: '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(.[0-9]{3})Z',
-  type4: '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}',
-  type5: '(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday)\\,\\ (January|February|March|April|May|June|July|August|September|October|November|December)\\ [0-9]{1,2},\\ [0-9]{4}\\ [0-9]{1,2}:[0-9]{2}\\ (AM|PM)',
-  type6: '((Sun|Mon|Tue|Wed|Thu|Fri|Sat)\\,\\ )?[a-zA-Z]{3}\\ [0-9]{1,2},\\ [0-9]{4}\\ [0-9]{1,2}:[0-9]{2}\\ (AM|PM)',
-};
-
-chrome.browserAction.onClicked.addListener(() => {
-  chrome.storage.local.get((storageData) => {
-    const { regexList, outputFormat } = storageData;
-    const applicableRegex = Object.keys(dateRegex).filter(regEx => regexList[regEx]).map(regEx => dateRegex[regEx]);
-    const dateFormat = {
-      year: outputFormat.year ? 'numeric' : undefined,
-      month: outputFormat.month ? 'short' : undefined,
-      day: outputFormat.day ? '2-digit' : undefined,
-      hour: outputFormat.hour ? '2-digit' : undefined,
-      minute: outputFormat.minute ? '2-digit' : undefined,
-      second: outputFormat.second ? '2-digit' : undefined,
-      hour12: !outputFormat.hour24,
-      timeZone: outputFormat.utc ? 'utc' : Intl.DateTimeFormat().resolvedOptions().timeZone ,
-    }
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        action: 'replaceDate',
-        payload: {
-          applicableRegex,
-          dateFormat,
-          highlight: outputFormat.highlight,
-        }
-      },
-      (response) => {});
+chrome.browserAction.onClicked.addListener(function(activeTab) {
+  console.log(activeTab);
+  chrome.tabs.executeScript({
+    file: 'assets/contentscript.js',
+    allFrames : true,
+    matchAboutBlank: true,
+  });
+  chrome.browserAction.setBadgeBackgroundColor({
+    color: '#1AA260',
+    tabId: activeTab.id,
+  }, () => {
+    chrome.browserAction.setBadgeText({
+      text: '✓',
+      tabId: activeTab.id,
     });
   });
 });
--- contentscripts/contentscript.js
+++ contentscripts/contentscript.js
@@ -1,15 +1,25 @@
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-  const { action, payload } = msg;
-  switch(action) {
-    case 'replaceDate':
-      payload.applicableRegex.forEach(regEx => {
-        findAndReplace(regEx, payload.dateFormat, payload.highlight, document.body);
-      });
+const dateRegex = {
+  type0: '[0-9]{13}',
+  type1: '[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} (\\+|\\-)[0-9]{4}',
+  type2: '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z',
+  type3: '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(.[0-9]{3})Z',
+  type4: '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}',
+  type5: '(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday)\\,\\ (January|February|March|April|May|June|July|August|September|October|November|December)\\ [0-9]{1,2},\\ [0-9]{4}\\ [0-9]{1,2}:[0-9]{2}\\ (AM|PM)',
+  type6: '((Sun|Mon|Tue|Wed|Thu|Fri|Sat)\\,\\ )?[a-zA-Z]{3}\\ [0-9]{1,2},\\ [0-9]{4}\\ [0-9]{1,2}:[0-9]{2}\\ (AM|PM)',
+};
+
+const replace = (rawText, regex, highlight, replacementFormat) => {
+  try {
+    const matchedText = rawText.match(regex)[0];
+    let replacementText = new Date(isNaN(Number(matchedText)) ? matchedText : Number(matchedText)).toLocaleTimeString('en-US', replacementFormat);
+    return rawText.replace(regex, highlight ? `<mark>${replacementText}</mark>` : replacementText);
+  } catch(e) {
+    console.log('Couldn\'t convert a date');
+    return rawText;
   }
-});
+};
 
-
-function findAndReplace(searchText, replacementFormat, highlight, searchNode) {
+const findAndReplace = (searchText, replacementFormat, highlight, searchNode) => {
   /** This function was derived from https://j11y.io/javascript/find-and-replace-text-with-javascript/ */
   if (!searchText || typeof replacementFormat === 'undefined') {
     return;
@@ -42,15 +52,22 @@ function findAndReplace(searchText, replacementFormat, highlight, searchNode) {
     parent.insertBefore(frag, currentNode);
     parent.removeChild(currentNode);
   }
-}
+};
 
-function replace(rawText, regex, highlight, replacementFormat) {
-  try {
-    const matchedText = rawText.match(regex)[0];
-    let replacementText = new Date(isNaN(Number(matchedText)) ? matchedText : Number(matchedText)).toLocaleTimeString('en-US', replacementFormat);
-    return rawText.replace(regex, highlight ? `<mark>${replacementText}</mark>` : replacementText);
-  } catch(e) {
-    console.log('Couldn\'t convert a date');
-    return rawText;
+chrome.storage.local.get((storageData) => {
+  const { regexList, outputFormat } = storageData;
+  const applicableRegex = Object.keys(dateRegex).filter(regEx => regexList[regEx]).map(regEx => dateRegex[regEx]);
+  const dateFormat = {
+    year: outputFormat.year ? 'numeric' : undefined,
+    month: outputFormat.month ? 'short' : undefined,
+    day: outputFormat.day ? '2-digit' : undefined,
+    hour: outputFormat.hour ? '2-digit' : undefined,
+    minute: outputFormat.minute ? '2-digit' : undefined,
+    second: outputFormat.second ? '2-digit' : undefined,
+    hour12: !outputFormat.hour24,
+    timeZone: outputFormat.utc ? 'utc' : Intl.DateTimeFormat().resolvedOptions().timeZone,
   }
-}
+  applicableRegex.forEach(regEx => {
+    findAndReplace(regEx, dateFormat, outputFormat.highlight, document.body);
+  });
+});
