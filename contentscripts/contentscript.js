4e2a64b623ad610436557dedf9341199a4a1fe5b
acesmndr <acesmndr@gmail.com>
2020-05-10 13:19:13 +0545

    Add support for UTC date format

--- contentscripts/contentscript.js
+++ contentscripts/contentscript.js
@@ -1,17 +1,18 @@
 const dateRegex = {
-  type0: '[0-9]{13}',
+  type0: '[0-9]{12,13}',
   type1: '[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} (\\+|\\-)[0-9]{4}',
   type2: '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z',
   type3: '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(.[0-9]{3})Z',
   type4: '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}',
   type5: '(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday)\\,\\ (January|February|March|April|May|June|July|August|September|October|November|December)\\ [0-9]{1,2},\\ [0-9]{4}\\ [0-9]{1,2}:[0-9]{2}\\ (AM|PM)',
   type6: '((Sun|Mon|Tue|Wed|Thu|Fri|Sat)\\,\\ )?[a-zA-Z]{3}\\ [0-9]{1,2},\\ [0-9]{4}\\ [0-9]{1,2}:[0-9]{2}\\ (AM|PM)',
+  type7: '[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} UTC',
 };
 
 const replace = (rawText, regex, highlight, replacementFormat) => {
   try {
     const matchedText = rawText.match(regex)[0];
-    let replacementText = new Date(isNaN(Number(matchedText)) ? matchedText : Number(matchedText)).toLocaleTimeString('en-US', replacementFormat);
+    let replacementText = new Date(regex === dateRegex['type0'] ? Number(matchedText) : matchedText).toLocaleTimeString('en-US', replacementFormat);
     return rawText.replace(regex, highlight ? `<mark>${replacementText}</mark>` : replacementText);
   } catch(e) {
     console.log('Couldn\'t convert a date');
